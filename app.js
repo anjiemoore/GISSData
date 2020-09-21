783d84f3f25db5c3dd0b4274cc8ce9b589c1e14c
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-09-21 17:04:31 -0600

    pushing chart data

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-getData();
+const xAxis = [];
+
+chartCreation();
 
 async function getData() {
     const response = fetch('ZonAnn.Ts+dSST.csv');
@@ -8,10 +10,33 @@ async function getData() {
         table.forEach(row => {
             const columns = row.split(',');
             const year = columns[0];
+            xAxis.push(year);
             const nhTemp = columns[1];
             const shTemp = columns[2];
             console.log(year, nhTemp, shTemp);
         });
+};
 
-        
+async function chartCreation() {
+    await getData();
+    const ctx = document.getElementById('myChart').getContext('2d');
+
+const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        labels: xAxis,
+        datasets: [{
+            label: 'Global Average Temperature',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
 }
