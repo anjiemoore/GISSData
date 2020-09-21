dafc3862e4be2a58e892c30d41c5b987fd4fc47e
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-09-21 16:38:09 -0600

    tested using test.csv and now put in actual data, fixed naming conventions, split rows onto new line and on commas

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 getData();
 
 async function getData() {
-    const response = fetch('test.csv');
+    const response = fetch('ZonAnn.Ts+dSST.csv');
     const data = await (await response).text();
-    console.log(data);
+
+    const table = data.split('\n').slice(1);
+        table.forEach(row => {
+            const columns = row.split(',');
+            const year = columns[0];
+            const temp = columns[1];
+            console.log(year, temp);
+        });
 }
