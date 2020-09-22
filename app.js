21c85e1144d8008c116d42ed46dd8b3705a462e5
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-09-21 20:15:25 -0600

    finished project

--- app.js
+++ app.js
@@ -1,64 +1,78 @@
-const xYears = [];
-const yNhtemps = [];
-const yShtemps = [];
-
 chartCreation();
 
 async function getData() {
+    const xYears = [];
+    const yNhtemps = [];
+    const yShtemps = [];
+    
     const response = fetch('ZonAnn.Ts+dSST.csv');
     const data = await (await response).text();
 
     const table = data.split('\n').slice(1);
-        table.forEach(row => {
-            const columns = row.split(',');
-            const year = columns[0];
-            xYears.push(year);
-            const nhTemp = columns[1];
-            yNhtemps.push(parseFloat(nhTemp) + 14);
-            const shTemp = columns[2];
-            yShtemps.push(parseFloat(shTemp) + 14);
-        });
+    table.forEach(row => {
+        const columns = row.split(',');
+        const year = columns[0];
+        xYears.push(year);
+        const nhTemp = columns[1];
+        yNhtemps.push(parseFloat(nhTemp) + 14);
+        const shTemp = columns[2];
+        yShtemps.push(parseFloat(shTemp) + 14);
+    });
+
+    return { xYears, yNhtemps, yShtemps }
 };
 
 async function chartCreation() {
-    await getData();
+    const data = await getData();
     const ctx = document.getElementById('myChart').getContext('2d');
 
-const chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+    const chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
 
-    // The data for our dataset
-    data: {
-    labels: xYears,
-    datasets: [{
-        fill: false,
-        borderColor: 'rgb(0, 174, 255)',
-        label: 'Northern Hemisphere',
-        yAxisID: 'NH',
-        data: yNhtemps
-    }, {
-        fill: false,
-        label: 'Southern Hemisphere',
-        borderColor: 'rgb(255, 110, 0)',
-        yAxisID: 'SH',
-        data: yShtemps
-    }]
-    },
+        // The data for our dataset
+        data: {
+            labels: data.xYears,
+            datasets: [{
+                fill: false,
+                borderColor: 'rgb(52, 127, 196)',
+                label: 'Northern Hemisphere',
+                yAxisID: 'NH',
+                data: data.yNhtemps
+            }, {
+                fill: false,
+                label: 'Southern Hemisphere',
+                borderColor: 'rgb(166, 83, 107)',
+                yAxisID: 'SH',
+                data: data.yShtemps
+            }]
+        },
 
-    // Configuration options go here
-    options: {
-        scales: {
-          yAxes: [{
-            id: 'NH',
-            type: 'linear',
-            position: 'left',
-          }, {
-            id: 'SH',
-            type: 'linear',
-            position: 'right',
-          }]
+        // Configuration options go here
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                yAxes: [{
+                    id: 'NH',
+                    type: 'linear',
+                    position: 'left',
+                    ticks: {
+                        callback: function(value, index, values) {
+                            return value + "°C";
+                        }
+                    }
+                }, {
+                    id: 'SH',
+                    type: 'linear',
+                    position: 'right',
+                    ticks: {
+                        callback: function(value, index, values) {
+                            return value + "°C";
+                        }
+                    }
+                }]
+            }
         }
-      }
-});
+    });
 }
